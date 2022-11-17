7fc99954b9c0e18742d15126c56517b99836f4cf
lukedurham <durham35@marshall.edu>
2022-11-17 15:15:24 -0500

    Updated Version 11/17

--- authentication/urls.py
+++ authentication/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path(r'showdb/', views.showdb, name='showdb'),
     path('about/', views.about, name='about'),
     path(r'searchCars/', views.search_Cars, name='searchCars'),
-    path(r'update_cars/', views.update_cars, name='update-cars'),
+    path(r'update_cars/<CarID>', views.update_cars, name='update-cars'),
 
 ]
